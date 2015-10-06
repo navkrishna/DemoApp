6382d710b3fbf47f1113ad7239640c47fc3f4810
krrishnaaaa <krishna@pcsalt.com>
2015-10-06 22:00:33 +0530

    added first fragment

---
+++ app/src/main/java/nk/demoapp/FirstFragment.java
@@ -0,0 +1,20 @@
+package nk.demoapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by krishna on Oct 06, 2015
+ */
+public class FirstFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        return view;
+    }
+}
--- app/src/main/java/nk/demoapp/MainActivity.java
+++ app/src/main/java/nk/demoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package nk.demoapp;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        loadFirstFragment();
+    }
+
+    private void loadFirstFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, new FirstFragment())
+                .commit();
     }
 
     @Override
